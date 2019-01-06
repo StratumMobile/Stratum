6a35d72c6902650a79b798bb1f20fafadc112abf
Francis Wardley <fwardley90@gmail.com>
2019-01-06 18:40:16 +0100

    DeckSwiper added

--- client/App.js
+++ client/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import DeckSwiper from './components/deckswiper'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content"/>
+        <DeckSwiper />
       </View>
     );
   }
@@ -14,8 +16,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
