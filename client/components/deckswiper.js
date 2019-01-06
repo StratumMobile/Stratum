89d45d448e32479dee93015b424bcbd4b531ebe6
Francis Wardley <fwardley90@gmail.com>
2019-01-06 23:10:26 +0100

    images added

--- client/components/deckswiper.js
+++ client/components/deckswiper.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Swiper from 'react-native-deck-swiper'
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
 export default class DeckSwiper extends React.Component {
   render () {
+    console.log('HEIGHT IS ====', height, 'WIDTH IS ======', width)
     return (
     <View style={styles.container}>
         <Swiper
-            cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+            cards={cards}
             renderCard={(card) => {
                 return (
                     <View style={styles.card}>
-                        <Text style={styles.text}>{card}</Text>
+                        <View style={styles.testBox}></View>
+                        <Text style={styles.text}>{card.title}</Text>
+                        <Image style={styles.image} source={{url: card.image}}/>
                     </View>
                 )
             }}
@@ -19,12 +22,53 @@ export default class DeckSwiper extends React.Component {
             onSwipedAll={() => {console.log('onSwipedAll')}}
             cardIndex={0}
             backgroundColor={'#0e0e0e'}
+            disableBottomSwipe={true}
+            disableTopSwipe={true}
             stackSize= {3}>
         </Swiper>
     </View>
     )
+  }
 }
-}
+const { height, width } = Dimensions.get('window')
+
+const cards = [
+  {
+    title: 'DO',
+    id: 1,
+    image: 'http://lorempixel.com/300/300/people'
+  },
+  {
+    title: 'MORE',
+    id: 2,
+    image: 'http://lorempixel.com/300/300/people'
+  },
+  {
+    title: 'OF',
+    id: 3,
+    image: 'http://lorempixel.com/300/300/people'
+  },
+  {
+    title: 'WHAT',
+    id: 4,
+    image: 'http://lorempixel.com/300/300/people'
+  },
+  {
+    title: 'MAKES',
+    id: 5,
+    image: 'http://lorempixel.com/300/300/people'
+  },
+  {
+    title: 'YOU',
+    id: 6,
+    image: 'http://lorempixel.com/300/300/people'
+  },
+  {
+    title: 'HAPPY',
+    id: 7,
+    image: 'http://lorempixel.com/300/300/people'
+  }
+]
 
 const styles = StyleSheet.create({
   container: {
@@ -37,11 +81,24 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "#E8E8E8",
     justifyContent: "center",
-    backgroundColor: "white"
+    backgroundColor: "white",
+    alignItems: 'stretch',
   },
   text: {
+    flex: 1,
     textAlign: "center",
     fontSize: 50,
     backgroundColor: "transparent"
+  },
+  image: {
+    flex:2,
+    width: 100,
+    height: 100,
+  },
+  testBox: {
+    flex: 3,
+    borderColor: 'red',
+    borderWidth: 2,
+    backgroundColor: 'red',
   }
 });
